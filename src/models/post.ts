4e2161c944ec8fa0bd5ce8bd4eae1c70e4c90d75
vaibhav-kumarituple <vaibhav.kumar@ituple.com>
2023-12-04 22:36:01 +0530

    pre funtion is added

--- src/models/post.ts
+++ src/models/post.ts
@@ -7,7 +7,6 @@ const commentSchema = new mongoose.Schema({
     date: { type: Date, default: Date.now },
 });
 
-
 const postSchema: Schema<Post> = new mongoose.Schema({
     content: { type: String, required: true },
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
@@ -15,19 +14,20 @@ const postSchema: Schema<Post> = new mongoose.Schema({
     likes: [{ type: String }],
     bookmarks: [String],
     createdAt: { type: Date, default: Date.now },
-
     updatedAt: { type: Date, required: true },
 });
 
-postSchema.pre('findOne', function (next) {
+postSchema.pre('find', function (next) {
     this.populate({
         path: 'user',
         select: '_id username image',
     });
+
     this.populate({
         path: 'comments.commenter',
         select: '_id username image', // Adjust this as per your User schema
     });
+
     next();
 });
 
